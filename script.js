172f9cb3ebd902620f7898bc8920913da68ba292
Lucas Cassidori <82485483+devcassidori@users.noreply.github.com>
2024-04-17 17:45:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function subzero() {
+    var audio = document.getElementById("audiosubzero");
+    audio.play();
+}
+function scorpion() {
+    var audio = document.getElementById("audioscorpion");
+    audio.play();
+}
+function noob() {
+    var audio = document.getElementById("audionoob");
+    audio.play();
+}
+function smoke() {
+    var audio = document.getElementById("audiosmoke");
+    audio.play();
+}
+function reptile() {
+    var audio = document.getElementById("audioreptile");
+    audio.play();
+}
+function rain() {
+    var audio = document.getElementById("audiorain");
+    audio.play();
+}
+function ermac() {
+    var audio = document.getElementById("audioermac");
+    audio.play();
+}
+function tremor() {
+    var audio = document.getElementById("audiotremor");
+    audio.play();
+}
